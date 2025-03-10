4f45571f170c958857d3aad6826f04cbe476ebc4
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2025-03-11 01:06:53 +0530

    Update: Env

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
-
   return (
+    <>
    <h1 className='text-2xl'>Hello, Team</h1>
+   <button onClick={() => {
+    console.log(import.meta.env);
+    
+   }}>Click</button>
+   </>
   )
 }
 
